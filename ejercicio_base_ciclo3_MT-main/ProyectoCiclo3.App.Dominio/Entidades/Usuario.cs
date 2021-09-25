73652f8eb2db9a40975dd321c2a9c552abb4dfd8
Julian <julamontufar@misena.edu.co>
2021-09-25 08:35:44 -0500

    commit 1.0

--- ejercicio_base_ciclo3_MT-main/ProyectoCiclo3.App.Dominio/Entidades/Usuario.cs
+++ ejercicio_base_ciclo3_MT-main/ProyectoCiclo3.App.Dominio/Entidades/Usuario.cs
@@ -6,7 +6,7 @@ namespace ProyectoCiclo3.App.Dominio{
         public int apellidos { get; set; }
         public string direccion { get; set; }
         public string telefono { get; set; }
-        public string  mail { get; set; }
+        
         
     }
 
