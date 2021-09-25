cdd749b2720da1dc2961e056dcb0a7eb23407df9
Julian <julamontufar@misena.edu.co>
2021-09-25 08:39:39 -0500

    commit 1.1

--- ejercicio_base_ciclo3_MT-main/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositoriosUsuario.cs
+++ ejercicio_base_ciclo3_MT-main/ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositoriosUsuario.cs
@@ -1,3 +1,8 @@
+epositorioBuses.cs
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
 namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
     public interface RepositoriosUsuario
@@ -7,7 +12,9 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
          {
              usuarios = new List<Usuario>()
              {
-                 new Usuario()
+                 new Usuario(1,"Sofia","Pedroza","calle 26b","3188166247"),
+                 new Usuario(2,"Julian","Montufar","calle 26b","3188166247"),
+                 new Usuario(2,"Nohora","Narvaez","calle 2b","3148166247")
 
              };
 
