335d8abfc7d88fe0bcbc12c2c7c8b48066e47bdc
Julian <julamontufar@misena.edu.co>
2021-09-28 21:59:36 -0500

    version estable

---
+++ ejercicio_base_ciclo3_MT-main/ProyectoCiclo3.App.Frontend/Pages/Usuario/Edit.chtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
+
+
+namespace ProyectoCiclo3.App.Frontend.Pages
+{
+    public class EditUsuarioModel : PageModel
+    {
+        
+        
+      private readonly RepositorioUsuario _repositorioUsuario;
+      [BindProperty]
+              public Usuario usuarios {get;set;}
+ 
+        public EditUsuarioModel (RepositorioUsuario _repositorioUsuario)
+       {
+            
+            this._repositorioUsuario=_repositorioUsuario;
+       }
+ 
+        public IActionResult OnGet(int usuarioid)
+        {
+                usuarios = _repositorioUsuario.GetUserWithId(usuarioid);
+                return Page();
+ 
+        }
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(usuarios.id>0)
+            {
+            usuarios = _repositorioUsuario.Update(usuarios);
+            }
+            return Page();
+        }
+
+    }
+}
