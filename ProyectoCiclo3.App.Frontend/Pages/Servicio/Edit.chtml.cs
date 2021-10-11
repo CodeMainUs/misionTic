c9a397b1fe74740b26b26cbd14fb88f7bc576881
Julian <julamontufar@misena.edu.co>
2021-10-11 11:09:20 -0500

    lecetores dinamicos 1.3

--- ProyectoCiclo3.App.Frontend/Pages/Servicio/Edit.chtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Servicio/Edit.chtml.cs
@@ -13,20 +13,29 @@ namespace ProyectoCiclo3.App.Frontend.Pages
     public class EditServicioModel : PageModel
     {
         
+      private readonly RepositorioUsuario _repositorioUsuario;
+      private readonly RepositorioEncomienda _repositorioEncomienda;
+      public IEnumerable<Usuario> Usuarios{get;set;}
+      public IEnumerable<Encomienda> Encomiendas{get;set;}
+        
         
       private readonly RepositorioServicio _repositorioServicio;
       [BindProperty]
               public Servicio servicios {get;set;}
  
-        public EditServicioModel (RepositorioServicio _repositorioServicio)
+        public EditServicioModel (RepositorioServicio _repositorioServicio,RepositorioUsuario _repositorioUsuario,RepositorioEncomienda _repositorioEncomienda)
        {
             
             this._repositorioServicio=_repositorioServicio;
+            this._repositorioUsuario=_repositorioUsuario;
+            this._repositorioEncomienda=_repositorioEncomienda;
        }
  
         public IActionResult OnGet(int usuarioid)
         {
                 servicios = _repositorioServicio.GetServiWithId(usuarioid);
+                Usuarios = _repositorioUsuario.GetAll();
+                Encomiendas = _repositorioEncomienda.GetAll();
                 return Page();
  
         }
